259206e56594e2650af435569c95810be8146759
KevMcEwan <kev_mcewan@hotmail.com>
2019-01-26 11:26:19 +0000

    refactoring completed

---
+++ hw_es6_features/hw_es6_instruments_start/client/src/helpers/pub_sub.js
@@ -0,0 +1,31 @@
+// class PubSub {
+//   // constructor(){}
+//
+//   publish(channel, payload){
+//     var event = new CustomEvent(channel, {
+//       detail: payload
+//     });
+//     document.dispatchEvent(event);
+//   };
+//
+//   subscribe(channel, callback){
+//     document.addEventListener(channel, callback);
+//   };
+//
+// };
+
+const PubSub = {
+  publish: function (channel, payload) {
+    var event = new CustomEvent(channel, {
+      detail: payload
+  });
+    document.dispatchEvent(event);
+  },
+
+  subscribe: function (channel, callback) {
+    document.addEventListener(channel, callback);
+  }
+};
+
+// module.exports = PubSub;
+export default PubSub;
